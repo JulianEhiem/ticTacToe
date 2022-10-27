5dc641c6ae834bceed488e7b3912ca5ce6ec1a18
JulianEhiem <jules.ehiem@gmail.com>
2022-10-27 14:05:32 -0400

    testing out table printing strategy

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,14 +1,16 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTacToe {
     static String[][] gameBoard = createGameBoard();
 
     public static void main(String[] args) {
-        System.out.println("Let's Play a game of TicTacToe!");
-        int user = choosePlayer();
-        boolean comp = computerChooses();
+//        System.out.println("Let's Play a game of TicTacToe!");
+//        int user = choosePlayer();
+//        boolean comp = computerChooses();
+        displayTestBoard();
 //        player chosen
 //        player one chooses position
 //        position is updated on board
@@ -50,5 +52,33 @@ public class TicTacToe {
 
     public static boolean computerChooses() {
         return true;
+    }
+
+    public static void displayTestBoard() {
+//        int [][] board = new int[][]{{1, 2, 3}, {4,5,6},{7,8,9}};
+        String [][] board = new String[3][3];
+
+        board[0][0] = "A";
+        board[0][1] = "B";
+        board[0][2] = "C";
+        board[1][0] = "D";
+        board[1][1] = "E";
+        board[1][2] = "F";
+        board[2][0] = "G";
+        board[2][1] = "H";
+        board[2][2] = "I";
+
+
+//        TODO: figure out why it prints 3 tables and solve it
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(Arrays.toString(board[j]) + ((i%2 == 0) ? "\n" : ""));
+            }
+        }
+
+
+
+//        System.out.print(Arrays.deepToString(board));
     }
 }
